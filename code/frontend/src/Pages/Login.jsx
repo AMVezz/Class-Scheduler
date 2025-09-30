ca9ff1d426b57d3b172d5474a2b6d7d373a73988
Matthew Barnes <matthew.m.barnes@wsu.edu>
2025-09-29 20:49:05 -0700

    Main logic for Login.jsx done, just need to implement a backend

--- code/frontend/src/Pages/Login.jsx
+++ code/frontend/src/Pages/Login.jsx
@@ -1,12 +1,50 @@
+/*
+			Using React Hook Form (library) to make things a little easier here. 
+*/
+
 import { useState, useEffect } from 'react';
 import {useForm} from 'react-hook-form'
 
 function Login() {
 	
+	
+	const {
+		//hooks from react-hook-form library below
+
+		register, //connects input fields.
+		handleSubmit, //wraps onSubmit so validation runs first
+		formState: {errors} //holds validation error msg's
+	} = useForm; 
+
+
+	//onSubmit runs when form is submitted successfuly. 
+	const onSubmit = (data) =>{
+		console.log("Form data: ", data); //we'll send data to backend when thats set up. data = (email+password)
+	}
+	
 	return (
-		<div>
-			<h1> Login Page </h1>
-		</div>
+		<form>
+			onSubmit={handleSubmit(onsubmit)}
+			<input>
+			type="email"
+			placeholder="Email"
+			{...register("email", {required: true})}
+			</input>
+			{errors.email && <span>Email is required!</span>}
+
+			<input>
+			type="password"
+			placeholder="Password"
+			{...register("password", {required: true})}
+			</input>
+			{errors.password && <span>Password is required!</span>}
+
+			<button type="submit">
+				Login
+			</button>
+		</form>
 	)
 }
+
+
 export default Login;
