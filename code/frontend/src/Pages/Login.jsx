81ebdb2ee986a20fdca0bf22d647ca680dd461ac
Matthew Barnes <matthew.m.barnes@wsu.edu>
2025-09-29 21:01:01 -0700

    Basic login.jsx frontend stuff is done.

--- code/frontend/src/Pages/Login.jsx
+++ code/frontend/src/Pages/Login.jsx
@@ -14,36 +14,34 @@ function Login() {
 		register, //connects input fields.
 		handleSubmit, //wraps onSubmit so validation runs first
 		formState: {errors} //holds validation error msg's
-	} = useForm; 
+	} = useForm(); 
 
 
 	//onSubmit runs when form is submitted successfuly. 
 	const onSubmit = (data) =>{
-		console.log("Form data: ", data); //we'll send data to backend when thats set up. data = (email+password)
+		console.log("Form data: ", data); //we'll send data to backend API when thats set up. data = (email+password)
 	}
 	
 	return (
-		<form>
-			onSubmit={handleSubmit(onsubmit)}
-			<input>
+		<form onSubmit={handleSubmit(onSubmit)}>
+		  <input
 			type="email"
 			placeholder="Email"
-			{...register("email", {required: true})}
-			</input>
-			{errors.email && <span>Email is required!</span>}
-
-			<input>
+			{...register("email", { required: true })}
+		  />
+		  {errors.email && <span>Email is required!</span>}
+	
+		  <input
 			type="password"
 			placeholder="Password"
-			{...register("password", {required: true})}
-			</input>
-			{errors.password && <span>Password is required!</span>}
-
-			<button type="submit">
-				Login
-			</button>
+			{...register("password", { required: true })}
+		  />
+		  {errors.password && <span>Password is required!</span>}
+	
+		  <button type="submit">Login</button>
 		</form>
-	)
+	  );
+	
 }
 
 
